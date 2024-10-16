fd50d6a110616753339b9efb13185a9dc6f7de97
schnell-auswahl <144453809+schnell-auswahl@users.noreply.github.com>
2024-10-16 17:04:30 +0200

    Wert wird jetzt in Valuewatchnote aus formel berechnet. to do: überall sonst weglassen, uv value mitgeben

--- CustWatchNodeValue.js
+++ CustWatchNodeValue.js
@@ -6,20 +6,70 @@ export function _CustWatchNodeValue() { return(
         this.bgcolor = "#FFFFFF"; //Hintergrundfarbe
 
 
-        this.addInput("value", 0, { label: "" });
-        this.value = 0;
+        this.addInput("","object");
+        //this.addInput("UV", "object");
+        this.inputData = 0;
         this.title = "Wert";
         this.desc = "Show value of input";
+        
+        this.properties = {
+          FormulaFromInput: "",   
+          uvValueFromInput: 0,
+          uvNameFromInput: "", 
+          displayValue: 0
+        };
+
+
+
+        this.oldFormula = "";
+        this.olduvValue = 0;
+        this.olduvNameFromInput = "";
+
+       
+
       }
   
       onExecute() {
-        if (this.inputs[0]) {
-            this.value = this.getInputData(0);
+
+   // Eingabedaten holen
+    //const inputData = this.getInputData(0);
+
+    // Ausgabe der empfangenen Eingabedaten in der Konsole
+    //console.log("Empfangene Eingabedaten:", inputData);
+
+
+        if (this.getInputData(0)) {
+          this.inputData = this.getInputData(0);
+          this.properties.FormulaFromInput = this.inputData["rightSide"];
+          this.properties.uvValueFromInput = this.inputData["value"]; // Gegen UV Value tauschen
+          this.properties.uvNameFromInput = this.inputData["uvName"]; 
+
+          if ( this.oldFormula != this.properties.FormulaFromInput || this.olduvValue != this.properties.uvValueFromInput || this.olduvNameFromInput != this.properties.uvNameFromInput) {
+           
+            try {
+              this._func = new Function(this.properties.uvNameFromInput,"return " + this.properties.FormulaFromInput); // Erstelle neue Funktion -> To Do funktion vorher sicher machen
+
+              this.properties.displayValue = this._func(this.properties.uvValueFromInput); // Wert aus Funktion ausrechen
+              //console.log("this._func lautet",this._func);
+              //console.log("rechnentest",this._func(1));
+              this.boxcolor = null; // Zurücksetzen der Farbe bei Erfolg
+
+              this.oldFormula = this.properties.FormulaFromInput;
+              this.olduvValue = this.properties.uvValueFromInput;
+              this.olduvNameFromInput = this.properties.uvNameFromInput;
+            }
+            catch (err) {
+              console.error("Fehler in der Formel:", err); // Fehlerbehandlung bei Problemen mit der Formel
+              this.boxcolor = "red"; // Fehlerfarbmarkierung
+            }
+
+
+          }  
         }
       };
   
       getTitle() {
-        if (this.flags.collapsed) {
+        if (this.flags.collapsed) { //Nochmal checken was das heißt
             return this.inputs[0].label;
         }
         return this.title;
@@ -28,14 +78,12 @@ export function _CustWatchNodeValue() { return(
       toString = function(o) {
         if (o == null) {
             return "";
-        // } else if (o.constructor === Number) {
-        //     return o.toFixed(3);
-        } else if (!o["value"] || o["value"] == null) {
-            return "Fehler";
-        } else {
-            // return String(o);
-            // Math.round((num + Number.EPSILON) * 100) / 100
-            var num = o["value"];
+        } 
+        //else if (!o["value"] || o["value"] == null) {
+          //  return "Fehler";
+        //}
+         else {
+            var num = o;
             num = Math.round((num + Number.EPSILON) * 100) / 100;
             return num;
         }
@@ -43,7 +91,7 @@ export function _CustWatchNodeValue() { return(
   
       onDrawBackground = function(ctx) {
         //show the current value
-        this.inputs[0].label = this.toString(this.value);
+        this.inputs[0].label = this.toString(this.properties.displayValue);
         // this.inputs[0].label = "Hi Nico"
       };
     }
